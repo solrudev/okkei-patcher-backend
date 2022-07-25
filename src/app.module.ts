de653f11a00401963c0ab4c726cddea4a01c706a
Ilya Fomichev <fomil.music@gmail.com>
2022-07-26 04:42:10 +0700

    async typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,21 +13,31 @@ import { PatcherModule } from "./modules/patcher/patcher.module";
 		ConfigModule.forRoot({
 			envFilePath: `.${process.env.NODE_ENV}.env`
 		}),
-		TypeOrmModule.forRoot({
-			type: "postgres",
-			host: process.env.POSTGRES_HOST,
-			port: Number(process.env.POSTGRES_PORT),
-			username: process.env.POSTGRES_USERNAME,
-			password: process.env.POSTGRES_PASSWORD,
-			database: process.env.POSTGRES_DATABASE,
-			ssl: { rejectUnauthorized: false },
-			entities: [
-				File,
-				EnglishPatchFile,
-				PatcherVersion,
-				PatcherChange
-			],
-			synchronize: Boolean(process.env.SYNCHRONIZE)
+		TypeOrmModule.forRootAsync({
+			useFactory: () => {
+				let sslOptions = {};
+				if (process.env.NODE_ENV == "production") {
+					sslOptions = {
+						ssl: { rejectUnauthorized: false }
+					};
+				}
+				return {
+					type: "postgres",
+					host: process.env.POSTGRES_HOST,
+					port: Number(process.env.POSTGRES_PORT),
+					username: process.env.POSTGRES_USERNAME,
+					password: process.env.POSTGRES_PASSWORD,
+					database: process.env.POSTGRES_DATABASE,
+					...sslOptions,
+					entities: [
+						File,
+						EnglishPatchFile,
+						PatcherVersion,
+						PatcherChange
+					],
+					synchronize: process.env.NODE_ENV == "development"
+				};
+			}
 		}),
 		PatchModule,
 		PatcherModule
