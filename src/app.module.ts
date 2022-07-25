4d6ba085c57bf418da13408645fc507a4f3fda4e
Ilya Fomichev <fomil.music@gmail.com>
2022-07-26 04:56:07 +0700

    use database url instead of credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,7 @@ import { PatcherModule } from "./modules/patcher/patcher.module";
 				}
 				return {
 					type: "postgres",
-					host: process.env.POSTGRES_HOST,
-					port: Number(process.env.POSTGRES_PORT),
-					username: process.env.POSTGRES_USERNAME,
-					password: process.env.POSTGRES_PASSWORD,
-					database: process.env.POSTGRES_DATABASE,
+					url: process.env.DATABASE_URL,
 					...sslOptions,
 					entities: [
 						File,
