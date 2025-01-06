065948f46e31b91215087c874ab9c458dcd0bf3c
Ilya Fomichev <fomil.music@gmail.com>
2025-01-06 22:45:28 +0500

    extract local variable

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,6 +1,7 @@
 export function groupBy<T, K extends keyof any>(arr: T[], key: (i: T) => K): Record<K, T[]> {
 	return arr.reduce((groups, item) => {
-		(groups[key(item)] ||= []).push(item);
+		const group = groups[key(item)] ||= [];
+		group.push(item)
 		return groups;
 	}, {} as Record<K, T[]>);
 }
